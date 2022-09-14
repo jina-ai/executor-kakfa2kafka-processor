7bb816d7d18edb31ee83952c910dec783980d6ab
Girish Chandrashekar <girish.chandrashekar@jina.ai>
2022-09-14 15:04:45 +0200

    test: add flow integration test

---
+++ tests/integration/__init__.py
@@ -0,0 +1,5 @@
+import pytest
+from docarray import Document, DocumentArray
+from jina import Flow
+
+from run_kafka2kafka_flow import print_messages_in_topic
---
+++ tests/integration/test_flow.py
@@ -0,0 +1,52 @@
+import json
+import time
+
+import pytest
+from docarray import Document
+from jina import Flow
+from kafka import KafkaConsumer
+from kafka2kafka import KafkaToKafka
+
+
+def test_flow(
+    docker_compose,
+    bootstrap_servers,
+    consumer_topic,
+    producer_topic,
+    num_messages,
+    batch_size,
+):
+    f = Flow().add(
+        uses=KafkaToKafka,
+        env={
+            'BOOTSTRAP_SERVERS': bootstrap_servers,
+            'CONSUMER_TOPIC': consumer_topic,
+            'PUBLISH_TOPIC': producer_topic,
+            'BATCH_SIZE': batch_size,
+        },
+    )
+
+    with f:
+        f.post(on='/')
+        for _ in range(12):
+            f.post(on='/')
+
+        time.sleep(2)
+
+    consumer = KafkaConsumer(
+        producer_topic,
+        group_id='console-consumer',
+        bootstrap_servers=bootstrap_servers,
+        auto_offset_reset='earliest',
+        value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+        consumer_timeout_ms=3000,
+    )
+
+    topic_partitions = consumer.poll(
+        timeout_ms=0, max_records=num_messages, update_offsets=True
+    )
+    consumer_records = []  # flatten all consumer records
+    for crs in topic_partitions.values():
+        consumer_records.extend(crs)
+
+    assert len(consumer_records) == num_messages
